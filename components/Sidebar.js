fc9e8957071ef4f6a4e11e8130186484b9e79692
srotoswini <srotoswini.dev@gmail.com>
2022-06-27 01:20:18 +0530

    .

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import { HomeIcon, SearchIcon,
     LibraryIcon, PlusCircleIcon, HeartIcon, RssIcon, } from "@heroicons/react/outline"
+import { signOut, useSession } from "next-auth/react"
+
 
 function Sidebar() {
+  
+  const { data: session, status } = useSession()
+
+  console.log(session)
+
   return (
-    <div className='text-gray-500 p-5 text-sm border-r border-gray-900'>
+    <div className='text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen'>
         <div className="space-y-4">
+
+        <button className="flex items-center space-x-2 hover:text-white hover" onClick={() => signOut()}>
+            <HeartIcon className="h-5 w-5" />
+            <p>Log Out</p> 
+            </button> 
+
             <button className="flex items-center space-x-2 hover:text-white hover">
             <HomeIcon className="h-5 w-5" />
             <p>Home</p> 
