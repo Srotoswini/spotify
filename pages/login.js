b31fc4591e9886763223193b5d39fbf70590dc48
srotoswini <srotoswini.dev@gmail.com>
2022-06-27 01:20:42 +0530

    .

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import { getProviders, signIn, provider } from "next-auth/react";
 
-function Login() {
+function Login({ providers }) {
   return (
-    <div>Login</div>
-  )
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+      <img className="w-52 mb-5" src="https:links.papareact.com/9xl" alt="" />
+
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button
+            className="bg-[#18d860] text-white p-5 rounded-lg"
+            onClick={() =>
+              signIn(provider.id, {
+                callbackUrl: "/",
+              })
+            }
+          >
+            Login With {provider.name}
+          </button>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default Login
+export default Login;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: {
+      providers,
+    },
+  };
+}
