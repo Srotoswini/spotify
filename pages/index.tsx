f25670d9f8bae603b50a3bb6996008a9f60dfd03
srotoswini <srotoswini.dev@gmail.com>
2022-06-26 14:28:03 +0530

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,22 +4,20 @@ import Sidebar from '../components/Sidebar'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="bg-black h-screen overflow-hidden">
       <Head>
         <title>Spotify</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main>
+        {/* sidebar */}
+        <Sidebar />
+        {/* center */}
+      </main>
 
-{/* <h1></h1> */}
-<main>
-  {/* sidebar */}
-  <Sidebar />
-  {/* center */}
-</main>
-
-<div> {/* player */} </div>
-    </div>
-  )
-}
+      <div> {/* player */} </div>
+          </div>
+        )
+      }
 
 export default Home
