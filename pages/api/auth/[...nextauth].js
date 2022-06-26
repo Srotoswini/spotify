75d3d3b2c5a7a0aff36dd62cae65135bbc67c28e
srotoswini <srotoswini.dev@gmail.com>
2022-06-26 14:28:16 +0530

    .

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth"
+import SpotifyProvider from "next-auth/providers/spotify"
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+})
