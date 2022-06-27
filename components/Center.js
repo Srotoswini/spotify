3cd25b17b917aa3b75849061b2faaf163106380f
srotoswini <srotoswini.dev@gmail.com>
2022-06-27 13:43:50 +0530

    .

--- components/Center.js
+++ components/Center.js
@@ -1,9 +1,30 @@
 import React from "react";
 import { useSession } from "next-auth/react";
 import { ChevronDownIcon } from "@heroicons/react/outline";
+import { useState, useEffect } from "react"
+import { shuffle } from "lodash"
+
+const colors = [
+  "from-indigo-500",
+  "from-blue-500",
+  "from-green-500",
+  "from-red-500",
+  "from-yellow-500",
+  "from-pink-500",
+  "from-purple-500",
+  ""
+]
+
+
 
 function Center() {
   const { data: session } = useSession();
+  const [color, setColor] = useState(null)
+
+  useEffect(() => {
+    setColor(shuffle(colors).pop())
+  }, [])
+
   return (
     <div className="flex-grow text-white">
       <header className="absolute top-5 right-8">
@@ -18,7 +39,7 @@ function Center() {
         </div>
       </header>
 
-      <section className="flex items-end space-x-7 bg-gradient-to-b to-black from-red-500 h-80 text-white p-8"></section>
+      <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8`}></section>
     </div>
   );
 }
