2ab896d908cbc95cb605fe314c768a11b197b6c6
srotoswini <srotoswini.dev@gmail.com>
2022-06-27 17:06:07 +0530

    .

--- hooks/useSpotify.js
+++ hooks/useSpotify.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import { useEffect } from "react"
-import { createNonNullExpression } from 'typescript'
+import { signIn, useSession } from "next-auth/react"
+import SpotifyWebApi from "spotify-web-api-node"
+
+const spotifyApi = new SpotifyWebApi ({
+  clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+  clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+})
 
 function useSpotify() {
 
     const { data: session, status } = useSession()
 
     useEffect(() => {
+        if (session) {
+          if (session.error === "RefreshAccessTokenError") {
+            signIn()
+          }
 
+          spotifyApi.setAccessToken(session.user.accessToken)
+        }
     }, [session])
 
-  return null
+  return spotifyApi
 }
 
 export default useSpotify
